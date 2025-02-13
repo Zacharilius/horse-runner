c752ea397ce58de899cdad8a3ed7c1fff7899716
Zachary Bensley <zabensley@gmail.com>
2025-02-13 07:16:20 -0500

    Add more Horse tests.

--- src/horse/index.test.ts
+++ src/horse/index.test.ts
@@ -1,33 +1,72 @@
 import Horse from './index';
 import Background from '../background';
 import Obstacle from '../obstacle';
+import Sound from '../sound';
 
-jest.mock('../image', () => {
-    return {
-        default: {
-            getImage: jest.fn().mockReturnValue('mock-image')
-        }
-    };
-});
+const SOUND_INDEX_FOR_NEIGHING = 2;
 
-jest.mock('../background', () => {
-    return {
-        default: jest.fn()
-    };
-});
+jest.mock('../background');
+jest.mock('../bounding-box');
+jest.mock('../obstacle');
+jest.mock('../sound');
 
-jest.mock('../sound', () => {
-    return {
-        default: jest.fn()
-    };
+const getCanvas = (): HTMLCanvasElement => {
+    return document.createElement('canvas');
+}
+
+const getHorse = (canvas: HTMLCanvasElement): Horse => {
+    const background = new Background(canvas)
+    const horse = new Horse(
+        canvas,
+        background,
+        new Obstacle(canvas, background),
+    );
+    return horse;
+}
+
+beforeEach(() => {
+    jest.clearAllMocks()
 });
 
 test('Horse - should initialize', () => {
-    const canvas = document.createElement('canvas');
-    const horse = new Horse(
-        canvas as unknown as HTMLCanvasElement,
-        {} as unknown as Background,
-        {} as unknown as Obstacle,
+    expect(getHorse(getCanvas())).toBeInstanceOf(Horse);
+});
+
+test('Horse - should play neighing sound when "n" key is pressed', () => {
+    const canvas = getCanvas();
+    const addEventListenerSpy = jest.spyOn(window, 'addEventListener');
+    getHorse(canvas);
+
+    // Creates 3 sounds.
+    expect(Sound).toHaveBeenCalledTimes(3);
+    const mockNeighingSound = (Sound as jest.Mock).mock.instances[SOUND_INDEX_FOR_NEIGHING];
+
+    const eventCallback = addEventListenerSpy.mock.calls[0][1] as EventListener;
+    eventCallback(new KeyboardEvent('keydown', { key: 'n' }));
+
+    expect(mockNeighingSound.play).toHaveBeenCalled();
+});
+
+test.only('Horse - should run when keys are pressed', () => {
+    const canvas = getCanvas() as jest.Mocked<HTMLCanvasElement>;
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+    jest.spyOn(canvas, 'addEventListener');
+    jest.spyOn(context, 'drawImage');
+
+    getHorse(canvas);
+    const eventCallback = canvas.addEventListener.mock.calls[0][1] as EventListener;
+    eventCallback(new Event('tick'));
+
+    expect(context.drawImage).toHaveBeenCalled();
+    expect(context.drawImage).toHaveBeenCalledWith(
+        expect.any(HTMLImageElement),
+        0,
+        0,
+        64,
+        48,
+        118,
+        72.5,
+        64,
+        48
     );
-    expect(horse).toBeInstanceOf(Horse);
 });
