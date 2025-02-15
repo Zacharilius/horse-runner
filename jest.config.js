f0517e45b9d83c9cfdc97e25548d602dbb652751
Zachary Bensley <zabensley@gmail.com>
2025-02-15 08:31:09 -0500

    Update jest coverageThresholds to match remote values.

--- jest.config.js
+++ jest.config.js
@@ -50,7 +50,7 @@ const config = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 77,
+      branches: 76,
       functions: 93,
       lines: 84,
       statements: 84,
