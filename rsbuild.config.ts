8b26db81840ae34581b947790e4d4d9b9da91e3b
Zachary Bensley <zabensley@gmail.com>
2025-03-08 07:45:16 -0500

    Update rsbuild config to show .ico and customize html page title.

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from '@rsbuild/core';
 
-export default defineConfig({});
+export default defineConfig({
+    html: {
+        title: 'horse-runner',
+        favicon: './src/horse-runner.ico',
+      },
+});
